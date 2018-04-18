01851f3963c79785ce72ed5604407d313e4722a3
LEO\LeO <nhan.mai@naustud.io>
2018-04-18 23:08:53 +0700

    add hook realtime

--- src/boot/notification.js
+++ src/boot/notification.js
@@ -1,21 +1,30 @@
-// /* --------------------------------------------------------
-// * Author Trần Đức Tiến
-// * Email ductienas@gmail.com
-// * Phone 0972970075
-// *
-// * Created: 2018-03-06 15:06:41
-// *------------------------------------------------------- */
+/* --------------------------------------------------------
+* Author Trần Đức Tiến
+* Email ductienas@gmail.com
+* Phone 0972970075
+*
+* Created: 2018-03-06 15:06:41
+*------------------------------------------------------- */
 
-// import es from 'event-stream';
+import es from 'event-stream';
 
-// export default function (app) {
-// 	const Notification = app.models.Notification;
+export default function (app) {
+	const TicketSelling = app.models.TicketSelling;
+	const TicketBuying = app.models.TicketBuying;
 
-// 	Notification.createChangeStream((err, changes) => {
-// 		if (err) {
-// 			console.log('err', err);
-// 		}
+	TicketSelling.createChangeStream((err, changes) => {
+		if (err) {
+			console.log('err', err);
+		}
 
-// 		changes.pipe(es.stringify()).pipe(process.stdout);
-// 	});
-// }
+		changes.pipe(es.stringify()).pipe(process.stdout);
+	});
+
+	TicketBuying.createChangeStream((err, changes) => {
+		if (err) {
+			console.log('err', err);
+		}
+
+		changes.pipe(es.stringify()).pipe(process.stdout);
+	});
+}
