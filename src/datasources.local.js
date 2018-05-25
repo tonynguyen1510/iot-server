a69dc12dfede72dbad3d2614b8c688bb69d7bf45
Ngo An Ninh <ninh.uit@gmail.com>
2018-05-25 08:28:34 +0700

    connect firebase

--- src/datasources.local.js
+++ src/datasources.local.js
@@ -18,6 +18,12 @@ module.exports = { // eslint-disable-line
 		'connector': 'mongodb',
 		'allowExtendedOperators': true
 	},
+	'firestore': {
+		'name': 'firestore',
+		'projectId': 'iot-demo-4dd93',
+		'clientEmail': 'firebase-adminsdk-vn8e2@iot-demo-4dd93.iam.gserviceaccount.com',
+		'privateKey': '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC8FKxl7eM8a8yW\nQLubAX5+hVZmXpEKcA0WG7f1T+LULBlMaYKw43ocWEfcz81K+ey/lymYiIR0oxdY\nOCdjze1nW36ozuDKUofXKmkwZRMJ33RT5pZwQvwIRn/vhW6PgLI45yAgxJyentvp\nMNYOFhtyf3GiFI8GOFCYgAQic80AM0fFUwTKifQuDF7j90Kdi0jQCe63SEFECDwK\nRfcH6nE3OkiUfs0Tk3xFk2dG2tycKLFnNh1qtb+kDuIZ/a+vhSKpg2ILf/GSAjqT\nxxOKshwcRTJPpzhkYQtfd7yJ5TyxksNh/BhfA4mk0A4PVDyAEn5aqUyQXaSzA0Ry\nnJ9zu/jjAgMBAAECggEAFWmQc62gxF+3c9cHJMSyRyrcpRyYtvn+cL5oyUELihaX\n2LxduHq6tmBnP2CEYZ1Q0pfIkLOLdOXzwe4+rxz3grVcFvwv2QjgAI3fr4v082z2\nXEObwUh2/hAx/4MO4mqHSnV8Zw6gkBR2OoxTSljs+LJBWG+uasmy2fHKcWECp/DR\n7UO8AAvFpkHpBeFQVUuTWqQ7I1PuV1ZIYQitb0+msJcRcgME/GVg1M6hFetDsoAz\nGW5S6G43cgf0YdM2zZRN2Pp4TM0Gg6AT224jKx8Lez9sXtDO8c5siXOPP4N4Jpio\nVpHh+ZzZoNvLurB07sll52/VIra94aKWAp6tA3aVFQKBgQDoClrSPFAWrWxWIYDq\n92xMitGO15kIR8HgKFmzbAxVZV2algMJpByc4srhKO0XldbRIIUJnDc8L/z5OYJQ\nuhP1lag/SP+CvByvhUZC1it+ZYt4I2TTjva6XPeI8/NNGQr8Yx6wNHsuDx3EZhkR\nzWVKlza4b6E5a1S4ZQNbbI3k/QKBgQDPgE8yAidNULJEQCkUAbwIe75AikPP3VJz\nrAqRRyOvg7uXWg+JAy2E8/Fou830IVrOEF/UKZXpgMBVIrdKad9s7psy+sWgnZhK\n6AGmThmocuPlqNqDn7NC4jX5aiasB45N6+OXv/rrajuUhEcLrxRoqNZFviotOKwA\nygiykUurXwKBgQCoCXej8BF/mq9t4G1fuzYuaAlCDFfCa1S3ZAExsflsji+vFxu0\nC6i85HBPhjfMcLJUV9yADSTi7U2hnkgE9fwmSO7Chbajkl6jrTwogriUnCzYH6mQ\nm2OJqnGIOEHlf1E5XP6IVDkwLbo81EMrZTcJQWBbSFvVHSkJL/2yY3aOcQKBgDRB\nI7B8Sll2EO6PGckoBqo8P1URfLJEEhd/I8oiGCKDEiKzU2Y1nqj0pb6yTNNF8xAh\nn4BWWKJNAiJuc6oRr2pHTF4yOMKR7WAvvPqcgGmjRTD4rM6o0QPu45jD2qlH3dZR\nqSb1+cJ/JjHofdu7Y3BX1F4L3aM1ulsYhcgyPtQRAoGAN87JeMHJhobHGDxyJuoi\nMjFYzS2YDjJt8FPbG38cw9vsyHI/jOUJvXf4GSfxikLF77eq3XSoIn9qxt/9X5i6\ncfwcyoq8M26xdoxh79sJOxPEhE72xGGbNPG0SCDUFTl7+cvS//2RbavmWlE/gEA/\nUwqTjb2cO/odZHqJX1JOPI4=\n-----END PRIVATE KEY-----\n'
+	},
 	'emailDs': {
 		'name': 'emailDs',
 		'connector': 'mail',
